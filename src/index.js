5f9a695feb1f57fcc9c8c5236a17a6949ccc7844
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-11-10 19:56:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,28 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-// import 'react-datepicker/dist/react-datepicker.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/drink_master-front">
+        <BrowserRouter basename="/fs-80-react-node-Dmytroukraine">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App/App';
-
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
